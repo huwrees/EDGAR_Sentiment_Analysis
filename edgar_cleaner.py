a86fb9f4aaa83073065a8e58e647b1f8eb14daca
Ousama Bousba Ghobrini <ousamabousbaghobrini@kubrickgroup.com>
2023-03-08 23:18:22 -0800

    files

---
+++ edgar_cleaner.py
@@ -0,0 +1,30 @@
+from bs4 import BeautifulSoup
+import re
+import os
+
+def clean_html_text(html_text):
+    cleantext = BeautifulSoup(html_text, "html.parser")
+    text = cleantext.get_text()
+    final_text = re.sub(r'\W+', " ", text)
+    return final_text
+
+
+def clean_html_files(source_folder, dest_folder):
+    if not os.path.exists(dest_folder):
+        os.makedirs(dest_folder)
+    
+    for filename in os.listdir(source_folder):
+        if filename.endswith(".html"):
+
+            with open(os.path.join(source_folder, filename), "r") as f:
+                html_text = f.read()
+            
+
+            clean_text = clean_html_text(html_text)
+            
+            with open(os.path.join(dest_folder, os.path.splitext(filename)[0] + ".txt"), "w") as f:
+                f.write(clean_text)
+
+            print('done')
+
+clean_html_files('C:\edgar_ec', r'C:\edgar_ec\txt.files')
